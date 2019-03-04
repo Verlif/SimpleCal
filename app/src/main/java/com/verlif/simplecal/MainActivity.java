751f9cf2334794f241b282e8c6fb013ce666e9ed
Verlif <1650825929@qq.com>
2019-03-04 15:38:03 +0800

    修复了得到结果后，输入数字后会显示混乱的bug 修复输入判定错误的bug 修改程序部分代码逻辑

--- app/src/main/java/com/verlif/simplecal/MainActivity.java
+++ app/src/main/java/com/verlif/simplecal/MainActivity.java
@@ -29,59 +29,66 @@ public class MainActivity extends AppCompatActivity {
         //获取按钮显示的文本信息
         String value = ((Button) findViewById(view.getId())).getText().toString();
         //结果输出
-        if (value.equals("=")) {
-            if (next != null && cal != null) {
-                //连续按“=”时自动补全运算
-                if (result != null) {
-                    textView.append(cal);
-                    textView.append(next + "");
-                    first = result;
+        switch (value) {
+            case "=":
+                if (next != null && cal != null) {
+                    //连续按“=”时自动补全运算
+                    if (result != null) {
+                        textView.append(cal);
+                        textView.append(next + "");
+                        first = result;
+                    }
+                    getResult();
+                    textView.append("\n" + result);
                 }
-                getResult();
-                textView.append("\n" + result);
-            }
-        }
-        //运算符号输入
-        else if (value.equals("+") || value.equals("-") || value.equals("*") || value.equals("/")) {
-            if (first == null && result == null) {
-                Toast.makeText(this, "no first number", Toast.LENGTH_LONG).show();
-                return;
-            }
-            //连续运算判定
-            if (next != null && first != null) {
-                getResult();
-                first = result;
-                textView.append("\n" + result);
-                cal = value;
-                textView.append(value);
-            }
-            else {
-                if (first == null)
+                break;
+            //运算符号输入
+            case "+":
+            case "-":
+            case "*":
+            case "/":
+                if (first == null && result == null) {
+                    Toast.makeText(this, "no first number", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                //连续运算判定
+                if (next != null && first != null) {
+                    getResult();
                     first = result;
-                next = null;
-                cal = value;
+                    textView.append("\n" + result);
+                    cal = value;
+                    textView.append(value);
+                } else {
+                    if (first == null)
+                        first = result;
+                    next = null;
+                    cal = value;
+                    textView.append(value);
+                }
+                result = null;
+                break;
+            //数字按下
+            default:
+                if (first == null && result != null)
+                    textView.append("\n");
+                if (first == null) {
+                    first = 0L;
+                    first += Integer.valueOf(value);
+                    cal = null;
+                    next = null;
+                    result = null;
+                } else if (cal == null) {
+                    first *= 10;
+                    first += Integer.valueOf(value);
+                } else {
+                    if (next == null)
+                        next = 0L;
+                    next *= 10;
+                    next += Integer.valueOf(value);
+                }
+                //显示框显示
                 textView.append(value);
-            }
-            result = null;
-        }
-        //数字按下
-        else {
-            if (result != null)
-                clear(view);
-            if (cal == null) {
-                if (first == null)
-                    first = Long.valueOf(0);
-                first *= 10;
-                first += Integer.valueOf(value);
-            }
-            else {
-                if (next == null || result == null)
-                    next = Long.valueOf(0);
-                next *= 10;
-                next += Integer.valueOf(value);
-            }
-            //显示框显示
-            textView.append(value);
+                break;
         }
 
     }
